3873c2e0e187a10b02dd86c6eef316614474164e
Sergio Bezerra <sergio.bezerra@outlook.com>
2021-03-15 14:23:22 -0300

    ajustes no pom

--- src/main/java/br/com/sbs/estacionamento/controller/VeiculoController.java
+++ src/main/java/br/com/sbs/estacionamento/controller/VeiculoController.java
@@ -1,18 +1,23 @@
 package br.com.sbs.estacionamento.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import br.com.sbs.estacionamento.entities.Veiculo;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping("/veiculos")
 public class VeiculoController {
 	
 	@GetMapping
-	public String hello(HttpServletRequest requisição) {
+	public List<Veiculo> findAll(HttpServletRequest requisiçao) {
+		List<Veiculo> lista = veiculoService.findAll();
+		
 		 
 		return "index";
 		

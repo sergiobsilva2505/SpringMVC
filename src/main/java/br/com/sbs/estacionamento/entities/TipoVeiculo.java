0b494304b7e7e64a934e02649fa2ac227f3ecb5d
Sergio Bezerra <sergio.bezerra@outlook.com>
2021-03-15 13:40:54 -0300

    Connection

---
+++ src/main/java/br/com/sbs/estacionamento/entities/TipoVeiculo.java
@@ -0,0 +1,7 @@
+package br.com.sbs.estacionamento.entities;
+
+public enum TipoVeiculo {
+	
+	CARRO, MOTO;
+
+}
--- src/main/java/br/com/sbs/estacionamento/entities/Veiculo.java
+++ src/main/java/br/com/sbs/estacionamento/entities/Veiculo.java
@@ -1,5 +1,33 @@
 package br.com.sbs.estacionamento.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Veiculo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false)
+	private String marca;
+
+	@Column(nullable = false)
+	private String modelo;
+
+	@Column(nullable = false)
+	private String cor;
+
+	@Column(nullable = false)
+	private String placa;
+
+	@Enumerated(EnumType.STRING)
+	private TipoVeiculo tipo;
 
 }

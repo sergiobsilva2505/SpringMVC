8d9e7f87bb13283afcdb19c85112a4fc7a6db8f9
Sergio Bezerra <sergio.bezerra@outlook.com>
2021-03-15 15:30:01 -0300

    Implementando hibernate

--- src/main/java/br/com/sbs/estacionamento/controller/VeiculoController.java
+++ src/main/java/br/com/sbs/estacionamento/controller/VeiculoController.java
@@ -14,13 +14,17 @@ import br.com.sbs.estacionamento.entities.Veiculo;
 @RequestMapping("/veiculos")
 public class VeiculoController {
 	
-	@GetMapping
-	public List<Veiculo> findAll(HttpServletRequest requisiçao) {
-		List<Veiculo> lista = veiculoService.findAll();
-		
-		 
+//	@GetMapping
+//	public List<Veiculo> findAll(HttpServletRequest requisiçao) {
+//		List<Veiculo> lista = veiculoService.findAll();
+//
+//		return "index";
+//
+//	}
+	
+	@GetMapping("/index")
+	public String showForm() {
 		return "index";
-		
 	}
 
 }
